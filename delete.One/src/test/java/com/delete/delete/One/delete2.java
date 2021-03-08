4e8355f6fd213b15d412e1129bd177af8ba3e871
ali.ashmi <ali.ashmi@ISSLT523.inubesolutions.net>
2021-03-08 15:11:03 +0530

    delete 2 added

---
+++ delete.One/src/test/java/com/delete/delete/One/delete2.java
@@ -0,0 +1,12 @@
+package com.delete.delete.One;
+
+public class delete2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("hello");
+
+	}
+
+}
