cf8326ed9175d223e6a05b5871aa46bb9e404222
akash.singh <akash.singh@ISSLT551.inubesolutions.net>
2021-03-08 14:21:05 +0530

    delete 1st

---
+++ delete.One/src/main/java/com/delete/delete/One/App.java
@@ -0,0 +1,13 @@
+package com.delete.delete.One;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
